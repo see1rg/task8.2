41c43b5e712d4c706d872fb0301f0895c55b83ba
see1rg <110130152+see1rg@users.noreply.github.com>
2023-05-10 13:14:29 +0300

    initial1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,5 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        String str = "aBcdaaadcba";
-        System.out.println(isPalindrome(str));
-    }
 
-    static boolean isPalindrome(String str) {
-        char[] charArray = str.toLowerCase().toCharArray();
-        boolean check = true;
-        int j = charArray.length - 1;
-        for (int i = 0; i < charArray.length / 2; i++) {
-            if (charArray[i] != charArray[j]) {
-                check = false;
-                break;
-            }
-            j--;
-        }
-        return check;
-    }
 }
