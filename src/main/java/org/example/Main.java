1ad007bfedf22ed2608f8bebf9b36ba6c4b05019
see1rg <110130152+see1rg@users.noreply.github.com>
2023-05-10 13:19:57 +0300

    initial2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+    //является ли слово палиндромом
     public static void main(String[] args) {
         String str = "aBcdaaadcba";
         System.out.println(isPalindrome(str));
